3265abb7ba741f719eb9d3b13df79656940ca4b0
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-03-10 11:18:02 +0000

    Added attributeusage

--- src/Dfe.ManageSchoolImprovement/Pages/TaskList/AddSupportingOrganisationContactDetails/EmailValidationAttribute.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/TaskList/AddSupportingOrganisationContactDetails/EmailValidationAttribute.cs
@@ -3,18 +3,16 @@ using System.Text.RegularExpressions;
 
 namespace Dfe.ManageSchoolImprovement.Frontend.Pages.TaskList.AddSupportingOrganisationContactDetails;
 
+[AttributeUsage(AttributeTargets.Property)]
 public class EmailValidationAttribute : ValidationAttribute
 {
-    private static readonly Regex EmailFormatRegex = new(@"^[^@\s]+@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$", RegexOptions.Compiled);
+    private static readonly Regex EmailFormatRegex = new(@"^[^@\s]+@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$", RegexOptions.Compiled, TimeSpan.FromMilliseconds(100));
 
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (value is string email)
+        if (value is string email && !EmailFormatRegex.IsMatch(email))
         {
-            if (!EmailFormatRegex.IsMatch(email))
-            {
-                return new ValidationResult("Email address must be in the correct format.");
-            }
+            return new ValidationResult("Email address must be in the correct format.");
         }
 
         return ValidationResult.Success;
