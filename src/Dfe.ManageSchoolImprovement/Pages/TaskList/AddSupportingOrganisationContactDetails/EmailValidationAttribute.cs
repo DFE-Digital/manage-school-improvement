839539604e2a17fc0d31fdbd43f5c5b4982f3cc3
mshakirdfe <muhammad.shakir@education.gov.uk>
2025-03-10 11:29:36 +0000

    Fixed incorrect email validation issue on Add supporting organisation contact details (#169)
    * Added unit tests
    
    * Fixed email validation issue.
    
    * Added attributeusage

---
+++ src/Dfe.ManageSchoolImprovement/Pages/TaskList/AddSupportingOrganisationContactDetails/EmailValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Dfe.ManageSchoolImprovement.Frontend.Pages.TaskList.AddSupportingOrganisationContactDetails;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class EmailValidationAttribute : ValidationAttribute
+{
+    private static readonly Regex EmailFormatRegex = new(@"^[^@\s]+@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$", RegexOptions.Compiled, TimeSpan.FromMilliseconds(100));
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is string email && !EmailFormatRegex.IsMatch(email))
+        {
+            return new ValidationResult("Email address must be in the correct format.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
