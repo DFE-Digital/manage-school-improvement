86b60f4a123e69627f834d4c03f02f761e467436
Dan Good <dangood84@me.com>
2025-03-12 14:42:35 +0000

    Fixed indentation on clickContinue() function

--- src/Dfe.ManageSchoolImprovement.CypressTests/cypress/pages/whichSchoolNeedsHelp.ts
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/pages/whichSchoolNeedsHelp.ts
@@ -1,33 +1,28 @@
 class WhichSchoolNeedsHelp {
+  public hasHeader(header: string): this {
+    header = "Which school needs help?";
 
-public hasHeader(header : string) : this {
-    
-    header = 'Which school needs help?'
-
-    cy.get('h1').contains(header)
+    cy.get("h1").contains(header);
 
     return this;
-}
+  }
 
-public withSchoolName(school: string): this {
+  public withSchoolName(school: string): this {
     cy.getById("SearchQuery").typeFast(school);
 
     // reassign school from 'Plym' to 'Plymouth Grove Primary School'
-    school = 'Plymouth Grove Primary School'
+    school = "Plymouth Grove Primary School";
 
-    cy.contains(school).click()
+    cy.contains(school).click();
 
     return this;
+  }
 
-    }
+  public clickContinue(): this {
+    cy.contains("Continue").click();
 
-public clickContinue(): this
-{
-    cy.contains('Continue').click()
-
-    return this
-}
-
+    return this;
+  }
 }
 
 const whichSchoolNeedsHelp = new WhichSchoolNeedsHelp();
