5cfd1029cb3dee2e5f338f32c88b32929d800691
Dan Good <dangood84@me.com>
2025-03-12 18:31:30 +0000

    Removed unnecessary const values from cypressConstants.ts, removed unnecessary Cypress env vars and made use of fixtures

--- src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/create-basic-project.cy.ts
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/create-basic-project.cy.ts
@@ -7,50 +7,66 @@ import path from "path";
 
 
 describe("User navigates to the rise landing page", () => {
-  let header = Cypress.env('header')
-  let school = Cypress.env('school')
-  let urn = Cypress.env('urn')
-  let localAuthority = Cypress.env('localAuthority')
-  let region = Cypress.env('region')
-  let schoolType = Cypress.env('schoolType')
-  let faithSchool = Cypress.env('faithSchool')
-  let ofstedRating = Cypress.env('ofstedRating')
-  let lastInspection = Cypress.env('lastInspection')
-  let pfi = Cypress.env('pfi')
+  let header;
+  let school;
+  let urn;
+  let localAuthority;
+  let region;
+  let schoolType;
+  let faithSchool;
+  let ofstedRating;
+  let lastInspection;
+  let pfi;
 
-    beforeEach(() => {
-        cy.login()
-        cy.url().should('contains', 'schools-requiring-improvement')   
+  before(() => {
+    cy.fixture('school-data').then((data) => {
+      header = data.header;
+      school = data.school;
+      urn = data.urn;
+      localAuthority = data.localAuthority;
+      region = data.region;
+      schoolType = data.schoolType;
+      faithSchool = data.faithSchool;
+      ofstedRating = data.ofstedRating;
+      lastInspection = data.lastInspection;
+      pfi = data.pfi;
     });
+  });
 
-    it("Should be able to add a school and add it to the list", () => {
-       riseHomePage
-         .AddSchool()
+  beforeEach(() => {
+    cy.login();
+    cy.url().should("contains", "schools-requiring-improvement");
+  });
 
-       cy.executeAccessibilityTests()
+  it("Should be able to add a school and add it to the list", () => {
+    riseHomePage.AddSchool();
 
-       whichSchoolNeedsHelp.hasHeader(header)
-                           .withSchoolName(school)
-                           .clickContinue()
+    cy.executeAccessibilityTests();
 
-       cy.executeAccessibilityTests()
+    whichSchoolNeedsHelp
+      .hasHeader(header)
+      .withSchoolName(school)
+      .clickContinue();
 
-       checkSchoolDetails.hasHeader(header)
-                         .hasSchoolName(school)
-                         .hasURN(urn)
-                         .hasLocalAuthority(localAuthority)
-                         .hasSchoolType(schoolType)
-                         .hasFaithSchool(faithSchool)
-                         .hasOfstedRating(ofstedRating)
-                         .hasLastInspection(lastInspection)
-                         .hasPFI(pfi)
+    cy.executeAccessibilityTests();
 
-       checkSchoolDetails.clickContinue()
+    checkSchoolDetails
+      .hasHeader(header)
+      .hasSchoolName(school)
+      .hasURN(urn)
+      .hasLocalAuthority(localAuthority)
+      .hasSchoolType(schoolType)
+      .hasFaithSchool(faithSchool)
+      .hasOfstedRating(ofstedRating)
+      .hasLastInspection(lastInspection)
+      .hasPFI(pfi);
 
-       riseHomePage.hasSchoolName(school)
-                   .hasURN(urn)
-                   .hasLocalAuthority(localAuthority)
-                   .hasRegion(region)
+    checkSchoolDetails.clickContinue();
 
-    });
+    riseHomePage
+      .hasSchoolName(school)
+      .hasURN(urn)
+      .hasLocalAuthority(localAuthority)
+      .hasRegion(region);
+  });
 });
