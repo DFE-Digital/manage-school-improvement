81e42b3168f5d6a55278f9aa62c20c13450f39ed
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-03-20 14:23:08 +0000

    removed js namespace

--- src/Dfe.ManageSchoolImprovement.Domain/Entities/SupportProject/SupportProject.cs
+++ src/Dfe.ManageSchoolImprovement.Domain/Entities/SupportProject/SupportProject.cs
@@ -1,6 +1,5 @@
 using Dfe.ManageSchoolImprovement.Domain.Common;
 using Dfe.ManageSchoolImprovement.Domain.ValueObjects;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Dfe.ManageSchoolImprovement.Domain.Entities.SupportProject;
 
