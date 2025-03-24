1c5ab25d2414c24455d96b43314385784a1f6d35
danielryannimble <danielryan@nimbleapproach.com>
2025-03-24 15:18:09 +0000

    rise adviser format

--- src/Tests/Dfe.ManageSchoolImprovement.Frontend.Tests/Validation/RiseAdviserEmailAttributeTests.cs
+++ src/Tests/Dfe.ManageSchoolImprovement.Frontend.Tests/Validation/RiseAdviserEmailAttributeTests.cs
@@ -8,9 +8,9 @@ namespace Dfe.ManageSchoolImprovement.Frontend.Tests.Validation
         private readonly RiseAdviserEmailAttribute _attribute = new RiseAdviserEmailAttribute();
 
         [Theory]
-        [InlineData("rise.john.doe@education.gov.uk")]
-        [InlineData("rise.jane-smith.doe@education.gov.uk")]
-        [InlineData("rise.john.doe-smith@education.gov.uk")]
+        [InlineData("john.doe-rise@education.gov.uk")]
+        [InlineData("jane-lee.smith-rise@education.gov.uk")]
+        [InlineData("john.doe-smith-rise@education.gov.uk")]
         public void IsValid_ValidEmail_ReturnsSuccess(string email)
         {
             var result = _attribute.GetValidationResult(email, new ValidationContext(new { }));
@@ -19,12 +19,12 @@ namespace Dfe.ManageSchoolImprovement.Frontend.Tests.Validation
 
         [Theory]
         [InlineData("john.doe@education.gov.uk")]
-        [InlineData("rise.johndoe@education.gov.uk")]
-        [InlineData("rise.john.doe@gmail.com")]
-        [InlineData("rise.john.doe@education.com")]
-        [InlineData("rise.john.doe@education.gov.uk.co")]
-        [InlineData("rise..john.doe@education.gov.uk")]
-        [InlineData("rise.john.doe.@education.gov.uk")]
+        [InlineData("johndoe-rise@education.gov.uk")]
+        [InlineData("john.doe-rise@gmail.com")]
+        [InlineData("john.doe-rise@education.com")]
+        [InlineData("john.doe-rise@education.gov.uk.co")]
+        [InlineData("john.doe--rise@education.gov.uk")]
+        [InlineData("john.doe.rise@education.gov.uk")]
         [InlineData("")]
         [InlineData(null)]
         public void IsValid_InvalidEmail_ReturnsValidationError(string? email)
