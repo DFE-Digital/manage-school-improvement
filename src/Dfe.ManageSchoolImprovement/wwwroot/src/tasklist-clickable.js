c0e0459df369034e2e9c6421590391ccc09d64d2
plockwood <paul.lockwood@education.gov.uk>
2025-04-22 11:13:21 +0100

    tasklist items clickable

---
+++ src/Dfe.ManageSchoolImprovement/wwwroot/src/tasklist-clickable.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".govuk-task-list__item--with-link").forEach(function (item) {
+        const link = item.querySelector("a.govuk-link");
+        if (link) {
+            item.style.cursor = "pointer";
+            item.addEventListener("click", function (event) {
+                if (!event.target.closest("a")) {
+                    window.location.href = link.href;
+                }
+            });
+        }
+    });
+});
