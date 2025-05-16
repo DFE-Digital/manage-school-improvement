56f8ca941043c138a9b99f5752a9326c1b1dedaf
plockwood <paul.lockwood@education.gov.uk>
2025-05-16 11:41:12 +0100

    added previous ofsted data added samesite attribute to cookies generate by app except for auth cookies override for govtag to remove max width

---
+++ src/Dfe.ManageSchoolImprovement/Services/Dtos/EstablishmentResponse.cs
@@ -0,0 +1,8 @@
+namespace Dfe.ManageSchoolImprovement.Frontend.Services.Dtos
+{
+    public class EstablishmentResponse
+    {
+        public MISEstablishmentResponse MISEstablishment { get; set; }
+
+    }
+}
