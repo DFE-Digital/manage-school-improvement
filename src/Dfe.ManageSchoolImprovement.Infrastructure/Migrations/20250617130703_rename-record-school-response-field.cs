8480a7bf648d7fab247edb24b80c5b1d720f5614
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2025-06-17 14:10:20 +0100

    rename db column

---
+++ src/Dfe.ManageSchoolImprovement.Infrastructure/Migrations/20250617130703_rename-record-school-response-field.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.ManageSchoolImprovement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamerecordschoolresponsefield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HasAcceptedTargetedSupport",
+                schema: "RISE",
+                table: "SupportProject",
+                newName: "HasAcknowledgedAndWillEngage")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SupportProjectHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "RISE")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HasAcknowledgedAndWillEngage",
+                schema: "RISE",
+                table: "SupportProject",
+                newName: "HasAcceptedTargetedSupport")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SupportProjectHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "RISE")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+    }
+}
