22c967fbbe61c39f4bdd978dcee3a75f9fb3efb6
plockwood <paul.lockwood@education.gov.uk>
2025-06-19 10:26:59 +0100

    - progress

--- src/Dfe.ManageSchoolImprovement/Pages/TaskList/ChoosePreferredSupportingOrganisation/index.cshtml.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/TaskList/ChoosePreferredSupportingOrganisation/index.cshtml.cs
@@ -8,24 +8,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.ManageSchoolImprovement.Frontend.Pages.TaskList.ChoosePreferredSupportingOrganisation;
 
-public class IndexModel(ISupportProjectQueryService supportProjectQueryService,ErrorService errorService, IMediator mediator) : BaseSupportProjectPageModel(supportProjectQueryService,errorService),IDateValidationMessageProvider
+public class IndexModel(ISupportProjectQueryService supportProjectQueryService, ErrorService errorService, IMediator mediator) : BaseSupportProjectPageModel(supportProjectQueryService, errorService), IDateValidationMessageProvider
 {
     [BindProperty(Name = "organisation-name")]
-    public string? OrganisationName  { get; set; }
-    
-    [BindProperty(Name = "id-number")]
-    
-    public string? IdNumber  { get; set; }
-    
-    public bool ShowError { get; set; }
-    
+    public string? OrganisationName { get; set; }
 
-    
+    [BindProperty(Name = "id-number")]
+
+    public string? IdNumber { get; set; }
+
+    public bool ShowError { get; set; }
+
     [BindProperty(Name = "date-support-organisation-chosen", BinderType = typeof(DateInputModelBinder))]
-    [DateValidation(Dfe.ManageSchoolImprovement.Frontend.Services.DateRangeValidationService.DateRange.PastOrToday)]
-    
-    public DateTime? DateSupportOrganisationChosen  { get; set; }
-    
+    [DateValidation(DateRangeValidationService.DateRange.PastOrToday)]
+
+    public DateTime? DateSupportOrganisationChosen { get; set; }
+
+    [BindProperty(Name = "complete-assessment")]
+    public bool CompleteAssessmentTool { get; set; }
+
     string IDateValidationMessageProvider.SomeMissing(string displayName, IEnumerable<string> missingParts)
     {
         return $"Date must include a {string.Join(" and ", missingParts)}";
@@ -35,39 +36,39 @@ public class IndexModel(ISupportProjectQueryService supportProjectQueryService,E
     {
         return $"Enter the preferred date for supporting organisation chosen";
     }
-    
+
     public async Task<IActionResult> OnGet(int id, CancellationToken cancellationToken)
     {
         await base.GetSupportProject(id, cancellationToken);
-        
+
         OrganisationName = SupportProject.SupportOrganisationName;
         IdNumber = SupportProject.SupportOrganisationIdNumber;
         DateSupportOrganisationChosen = SupportProject.DateSupportOrganisationChosen;
 
         return Page();
     }
-    
-    public async Task<IActionResult> OnPost(int id,CancellationToken cancellationToken)
+
+    public async Task<IActionResult> OnPost(int id, CancellationToken cancellationToken)
     {
-      
-        
+
+
         if (!ModelState.IsValid)
         {
             _errorService.AddErrors(Request.Form.Keys, ModelState);
             ShowError = true;
             return await base.GetSupportProject(id, cancellationToken);
         }
-        
-        var request = new SetChoosePreferredSupportingOrganisationCommand(new SupportProjectId(id),  OrganisationName ,IdNumber,DateSupportOrganisationChosen );
+
+        var request = new SetChoosePreferredSupportingOrganisationCommand(new SupportProjectId(id), OrganisationName, IdNumber, DateSupportOrganisationChosen);
 
         var result = await mediator.Send(request, cancellationToken);
-       
+
         if (result != true)
         {
             _errorService.AddApiError();
             return await base.GetSupportProject(id, cancellationToken);
         }
-        
+
         TaskUpdated = true;
         return RedirectToPage(@Links.TaskList.Index.Page, new { id });
     }
