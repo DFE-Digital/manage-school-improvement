aba1bb58a4d723d7cde9d1d7ba9780637ee9925b
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2025-06-23 13:48:12 +0100

    add funding band checkbox and radio buttons to review improvement plan task

--- src/Dfe.ManageSchoolImprovement/Pages/TaskList/ReviewTheImprovementPlan/Index.cshtml.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/TaskList/ReviewTheImprovementPlan/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Dfe.ManageSchoolImprovement.Application.SupportProject.Queries;
 using Dfe.ManageSchoolImprovement.Domain.ValueObjects;
 using Dfe.ManageSchoolImprovement.Frontend.Models;
 using Dfe.ManageSchoolImprovement.Frontend.Services;
+using Dfe.ManageSchoolImprovement.Frontend.ViewModels;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,7 +21,18 @@ namespace Dfe.ManageSchoolImprovement.Frontend.Pages.TaskList.ReviewTheImproveme
         [BindProperty(Name = "confirm-plan-cleared-by-rise")]
         public bool? ConfirmPlanClearedByRiseGrantTeam { get; set; } 
         
+        [BindProperty(Name = "funding-band")]
+        public string? FundingBand { get; set; }
+        
+        public bool? ConfirmFundingBand  { get; set; }
+        
         public string EmailAddress { get; set; } = "rise.grant@education.gov.uk";
+
+        public string ConfirmFundingBandLink { get; set; } = "http://www.google.com";
+        
+        public string FundingBandGuidanceLink { get; set; } = "https://www.google.com";
+        
+        public required IList<RadioButtonsLabelViewModel> SelectFundingBandRadioButtons { get; set; }
         
         public bool ShowError { get; set; }
         string IDateValidationMessageProvider.SomeMissing(string displayName, IEnumerable<string> missingParts)
@@ -39,6 +51,10 @@ namespace Dfe.ManageSchoolImprovement.Frontend.Pages.TaskList.ReviewTheImproveme
             DateImprovementPlanReceived = SupportProject.ImprovementPlanReceivedDate;
             ReviewImprovementAndExpenditurePlan = SupportProject.ReviewImprovementAndExpenditurePlan;
             ConfirmPlanClearedByRiseGrantTeam = SupportProject.ConfirmPlanClearedByRiseGrantTeam;
+
+            ConfirmFundingBand = false;
+
+            SelectFundingBandRadioButtons = GetRadioButtons();
             return Page();
         }
         
@@ -66,6 +82,37 @@ namespace Dfe.ManageSchoolImprovement.Frontend.Pages.TaskList.ReviewTheImproveme
 
             TaskUpdated = true;
             return RedirectToPage(@Links.TaskList.Index.Page, new { id });
+        }
+        
+        private IList<RadioButtonsLabelViewModel> GetRadioButtons()
+        {
+            var list = new List<RadioButtonsLabelViewModel>
+            {
+                new() {
+                    Id = "none",
+                    Name = "No funding required",
+                    Value = "No funding required"
+                },
+                new() {
+                    Id = "40000",
+                    Name = "Up to £40,000",
+                    Value = "Up to £40,000"
+                },
+                new()
+                {
+                    Id = "80000",
+                    Name = "Up to £80,000",
+                    Value = "Up to £80,000"
+                },
+                new()
+                {
+                    Id = "120000",
+                    Name = "Up to £120,000",
+                    Value = "Up to £120,000"
+                }
+            };
+
+            return list;
         }
         
     }
