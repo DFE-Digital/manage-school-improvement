2754e2272bb43d28b264db5924c8950c4d118747
plockwood <paul.lockwood@education.gov.uk>
2025-07-01 17:18:40 +0100

    fix for namespace

--- src/Dfe.ManageSchoolImprovement/Pages/EngagementConcern/EscalateEngagementConcern/EngagementConcernEscalationDetails.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/EngagementConcern/EscalateEngagementConcern/EngagementConcernEscalationDetails.cs
@@ -1,8 +1,10 @@
 namespace Dfe.ManageSchoolImprovement.Frontend.Pages.EngagementConcern.EscalateEngagementConcern
-public record EngagementConcernEscalationDetails
 {
-    public bool? ConfirmStepsTaken { get; init; }
-    public string? PrimaryReason { get; init; }
-    public string? Details { get; init; }
-    public DateTime? DateOfDecision { get; init; }
+    public record EngagementConcernEscalationDetails
+    {
+        public bool? ConfirmStepsTaken { get; init; }
+        public string? PrimaryReason { get; init; }
+        public string? Details { get; init; }
+        public DateTime? DateOfDecision { get; init; }
+    }
 }
