cf9db80bf9797b3b2dd3103b6dae477812f4e67b
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2025-07-22 11:36:55 +0100

    remove unused repository method

--- src/Dfe.ManageSchoolImprovement.Infrastructure/Repositories/SupportProjectRepository.cs
+++ src/Dfe.ManageSchoolImprovement.Infrastructure/Repositories/SupportProjectRepository.cs
@@ -163,13 +163,6 @@ namespace Dfe.ManageSchoolImprovement.Infrastructure.Repositories
             return await DefaultIncludes().SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
         }
 
-        public async Task<SupportProject?> GetSupportProjectByIdIgnoringFilters(SupportProjectId id, CancellationToken cancellationToken)
-        {
-            return await DefaultIncludes()
-                .IgnoreQueryFilters()
-                .SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
-        }
-
         public async Task<SupportProject?> GetSupportProjectByUrnIgnoringFilters(string urn, CancellationToken cancellationToken)
         {
             return await DefaultIncludes()
