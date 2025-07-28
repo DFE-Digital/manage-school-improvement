25a125a2a24a746a19a4c6a59edd62588c8b9b54
plockwood <paul.lockwood@nimbleapproach.com>
2025-07-28 13:12:05 +0100

    code smell fix

--- src/Dfe.ManageSchoolImprovement/ViewModels/TaskStatusViewModel.cs
+++ src/Dfe.ManageSchoolImprovement/ViewModels/TaskStatusViewModel.cs
@@ -447,13 +447,13 @@ public static class TaskStatusViewModel
     {
         var improvementPlan = supportProject.ImprovementPlans?.FirstOrDefault();
 
-        if (improvementPlan == null || improvementPlan.ImprovementPlanObjectives == null || improvementPlan.ImprovementPlanObjectives.Count() == 0)
+        if (improvementPlan == null || improvementPlan.ImprovementPlanObjectives == null || improvementPlan.ImprovementPlanObjectives.Count == 0)
         {
             return TaskListStatus.NotStarted;
         }
 
         if (improvementPlan.ImprovementPlanObjectives != null
-            && improvementPlan.ImprovementPlanObjectives.Count() > 0
+            && improvementPlan.ImprovementPlanObjectives.Count > 0
             && improvementPlan.ObjectivesSectionComplete is true)
         {
             return TaskListStatus.Complete;
