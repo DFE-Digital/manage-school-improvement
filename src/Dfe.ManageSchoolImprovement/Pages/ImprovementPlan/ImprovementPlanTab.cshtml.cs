945272a40a34db08446482ba8c028d50edac0190
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2025-07-30 11:10:36 +0100

    dynamically add objectives to summary cards

--- src/Dfe.ManageSchoolImprovement/Pages/ImprovementPlan/ImprovementPlanTab.cshtml.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/ImprovementPlan/ImprovementPlanTab.cshtml.cs
@@ -16,7 +16,23 @@ public class ImprovementPlanTabModel(
     
     public ImprovementPlanViewModel? ImprovementPlan { get; set; }
     
-    public List<ImprovementPlanObjectiveViewModel> Objectives { get; set; } = new List<ImprovementPlanObjectiveViewModel>();
+    public List<List<ImprovementPlanObjectiveViewModel>>Objectives { get; set; } = new();
+    
+    public List<ImprovementPlanObjectiveViewModel> QualityOfEducationObjectives =>
+        ImprovementPlan?.ImprovementPlanObjectives?.Where(o => o.AreaOfImprovement == "Quality of education").OrderBy(o => o.Order).ToList() ?? new();
+
+    public List<ImprovementPlanObjectiveViewModel> LeadershipAndManagementObjectives =>
+        ImprovementPlan?.ImprovementPlanObjectives?.Where(o => o.AreaOfImprovement == "Leadership and management").OrderBy(o => o.Order).ToList() ?? new();
+
+    public List<ImprovementPlanObjectiveViewModel> BehaviourAndAttitudesObjectives =>
+        ImprovementPlan?.ImprovementPlanObjectives?.Where(o => o.AreaOfImprovement == "Behaviour and attitudes").OrderBy(o => o.Order).ToList() ?? new();
+
+    public List<ImprovementPlanObjectiveViewModel> AttendanceObjectives =>
+        ImprovementPlan?.ImprovementPlanObjectives?.Where(o => o.AreaOfImprovement == "Attendance").OrderBy(o => o.Order).ToList() ?? new();
+
+    public List<ImprovementPlanObjectiveViewModel> PersonalDevelopmentObjectives =>
+        ImprovementPlan?.ImprovementPlanObjectives?.Where(o => o.AreaOfImprovement == "Personal development").OrderBy(o => o.Order).ToList() ?? new();
+
     public async Task<IActionResult> OnGetAsync(int id, CancellationToken cancellationToken)
     {
         ReturnPage = Links.SchoolList.Index.Page;
@@ -26,7 +42,28 @@ public class ImprovementPlanTabModel(
         // Ensure SupportProject is not null before accessing ImprovementPlans
         if (SupportProject?.ImprovementPlans != null)
         {
-            Objectives = SupportProject.ImprovementPlans.Objectives;
+            ImprovementPlan = SupportProject.ImprovementPlans.FirstOrDefault();
+
+            if (QualityOfEducationObjectives.Count != 0 ||
+                LeadershipAndManagementObjectives.Count != 0 ||
+                BehaviourAndAttitudesObjectives.Count != 0 ||
+                AttendanceObjectives.Count != 0 ||
+                PersonalDevelopmentObjectives.Count != 0)
+            {
+                Objectives = new List<List<ImprovementPlanObjectiveViewModel>>
+                {
+                    QualityOfEducationObjectives,
+                    LeadershipAndManagementObjectives,
+                    BehaviourAndAttitudesObjectives,
+                    AttendanceObjectives,
+                    PersonalDevelopmentObjectives,
+                };  
+            }
+            else
+            {
+                Objectives = new List<List<ImprovementPlanObjectiveViewModel>>();
+            }
+
         }
 
         return Page();
