ce2a65796a73a13c59ac8bfc47397577efd250e8
RichikaD <richika.test@gmail.com>
2025-07-30 16:11:04 +0100

    include improvement plan tab test

---
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/pages/improvementPlan.ts
@@ -0,0 +1,12 @@
+class ImprovementPlan {
+
+    hasHeading(expectedHeading: string): this {
+        cy.get('h2').should('contain.text', expectedHeading);
+        return this;
+    }
+
+}
+
+const improvementPlan = new ImprovementPlan();
+
+export default improvementPlan;
