c0153fb09957ad1f454f793486b65e1021f3d086
plockwood <paul.lockwood@nimbleapproach.com>
2025-10-01 11:24:26 +0100

    - added unit tests

--- src/Tests/Dfe.ManageSchoolImprovement.Domain.Tests/Entities/SupportProject/EngagementConcernTests.cs
+++ src/Tests/Dfe.ManageSchoolImprovement.Domain.Tests/Entities/SupportProject/EngagementConcernTests.cs
@@ -1,5 +1,3 @@
-using System;
-using Xunit;
 using Dfe.ManageSchoolImprovement.Domain.Entities.SupportProject;
 using Dfe.ManageSchoolImprovement.Domain.ValueObjects;
 
@@ -108,6 +106,7 @@ namespace Dfe.ManageSchoolImprovement.Domain.Tests.Entities.SupportProject
             Assert.Equal(dateOfDecision, concern.EngagementConcernEscalationDateOfDecision);
             Assert.Equal(warningNotice, concern.EngagementConcernEscalationWarningNotice);
         }
+
         [Fact]
         public void Constructor_WithNullValues_ShouldCreateValidInstance()
         {
@@ -123,6 +122,196 @@ namespace Dfe.ManageSchoolImprovement.Domain.Tests.Entities.SupportProject
             Assert.Null(concern.EngagementConcernResolved);
             Assert.Null(concern.EngagementConcernResolvedDetails);
             Assert.Null(concern.EngagementConcernResolvedDate);
+        }
+
+        [Fact]
+        public void SetInformationPowersDetails_ShouldUpdateProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+            var informationPowersInUse = true;
+            var informationPowersDetails = "Information powers were used to obtain necessary data";
+            var powersUsedDate = DateTime.Now;
+
+            // Act
+            concern.SetInformationPowersDetails(informationPowersInUse, informationPowersDetails, powersUsedDate);
+
+            // Assert
+            Assert.Equal(informationPowersInUse, concern.InformationPowersInUse);
+            Assert.Equal(informationPowersDetails, concern.InformationPowersDetails);
+            Assert.Equal(powersUsedDate, concern.PowersUsedDate);
+        }
+
+        [Fact]
+        public void SetInformationPowersDetails_WithNullValues_ShouldUpdateProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+
+            // Act
+            concern.SetInformationPowersDetails(null, null, null);
+
+            // Assert
+            Assert.Null(concern.InformationPowersInUse);
+            Assert.Null(concern.InformationPowersDetails);
+            Assert.Null(concern.PowersUsedDate);
+        }
+
+        [Fact]
+        public void SetInformationPowersDetails_WithFalseValue_ShouldUpdateProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+            var informationPowersInUse = false;
+            var informationPowersDetails = "Information powers were not required";
+            var powersUsedDate = DateTime.Now;
+
+            // Act
+            concern.SetInformationPowersDetails(informationPowersInUse, informationPowersDetails, powersUsedDate);
+
+            // Assert
+            Assert.Equal(informationPowersInUse, concern.InformationPowersInUse);
+            Assert.Equal(informationPowersDetails, concern.InformationPowersDetails);
+            Assert.Equal(powersUsedDate, concern.PowersUsedDate);
+        }
+
+        [Fact]
+        public void SetInterimExecutiveBoardCreated_ShouldUpdateProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+            var interimExecutiveBoardCreated = true;
+            var interimExecutiveBoardCreatedDetails = "Interim Executive Board was established to oversee school operations";
+            var interimExecutiveBoardCreatedDate = DateTime.Now;
+
+            // Act
+            concern.SetInterimExecutiveBoardCreated(
+                interimExecutiveBoardCreated,
+                interimExecutiveBoardCreatedDetails,
+                interimExecutiveBoardCreatedDate);
+
+            // Assert
+            Assert.Equal(interimExecutiveBoardCreated, concern.InterimExecutiveBoardCreated);
+            Assert.Equal(interimExecutiveBoardCreatedDetails, concern.InterimExecutiveBoardCreatedDetails);
+            Assert.Equal(interimExecutiveBoardCreatedDate, concern.InterimExecutiveBoardCreatedDate);
+        }
+
+        [Fact]
+        public void SetInterimExecutiveBoardCreated_WithNullValues_ShouldUpdateProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+
+            // Act
+            concern.SetInterimExecutiveBoardCreated(null, null, null);
+
+            // Assert
+            Assert.Null(concern.InterimExecutiveBoardCreated);
+            Assert.Null(concern.InterimExecutiveBoardCreatedDetails);
+            Assert.Null(concern.InterimExecutiveBoardCreatedDate);
+        }
+
+        [Fact]
+        public void SetInterimExecutiveBoardCreated_WithFalseValue_ShouldUpdateProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+            var interimExecutiveBoardCreated = false;
+            var interimExecutiveBoardCreatedDetails = "Interim Executive Board was not required";
+            var interimExecutiveBoardCreatedDate = DateTime.Now;
+
+            // Act
+            concern.SetInterimExecutiveBoardCreated(
+                interimExecutiveBoardCreated,
+                interimExecutiveBoardCreatedDetails,
+                interimExecutiveBoardCreatedDate);
+
+            // Assert
+            Assert.Equal(interimExecutiveBoardCreated, concern.InterimExecutiveBoardCreated);
+            Assert.Equal(interimExecutiveBoardCreatedDetails, concern.InterimExecutiveBoardCreatedDetails);
+            Assert.Equal(interimExecutiveBoardCreatedDate, concern.InterimExecutiveBoardCreatedDate);
+        }
+
+        [Fact]
+        public void SetInformationPowersDetails_CalledMultipleTimes_ShouldUpdateEachTime()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+
+            // First call
+            concern.SetInformationPowersDetails(true, "Initial details", DateTime.Now.AddDays(-1));
+
+            var newPowersInUse = false;
+            var newDetails = "Updated information powers details";
+            var newDate = DateTime.Now;
+
+            // Act - Second call
+            concern.SetInformationPowersDetails(newPowersInUse, newDetails, newDate);
+
+            // Assert
+            Assert.Equal(newPowersInUse, concern.InformationPowersInUse);
+            Assert.Equal(newDetails, concern.InformationPowersDetails);
+            Assert.Equal(newDate, concern.PowersUsedDate);
+        }
+
+        [Fact]
+        public void SetInterimExecutiveBoardCreated_CalledMultipleTimes_ShouldUpdateEachTime()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+
+            // First call
+            concern.SetInterimExecutiveBoardCreated(true, "Initial IEB details", DateTime.Now.AddDays(-1));
+
+            var newIebCreated = false;
+            var newDetails = "Updated IEB details";
+            var newDate = DateTime.Now;
+
+            // Act - Second call
+            concern.SetInterimExecutiveBoardCreated(newIebCreated, newDetails, newDate);
+
+            // Assert
+            Assert.Equal(newIebCreated, concern.InterimExecutiveBoardCreated);
+            Assert.Equal(newDetails, concern.InterimExecutiveBoardCreatedDetails);
+            Assert.Equal(newDate, concern.InterimExecutiveBoardCreatedDate);
+        }
+
+        [Fact]
+        public void SetInformationPowersDetails_DoesNotAffectOtherProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+            concern.SetEngagementConcernDetails("Original details", "Summary", DateTime.Now.AddDays(-2));
+            concern.SetInterimExecutiveBoardCreated(true, "IEB details", DateTime.Now.AddDays(-1));
+
+            var originalDetails = concern.EngagementConcernDetails;
+            var originalIebCreated = concern.InterimExecutiveBoardCreated;
+
+            // Act
+            concern.SetInformationPowersDetails(true, "Information powers details", DateTime.Now);
+
+            // Assert - Other properties remain unchanged
+            Assert.Equal(originalDetails, concern.EngagementConcernDetails);
+            Assert.Equal(originalIebCreated, concern.InterimExecutiveBoardCreated);
+        }
+
+        [Fact]
+        public void SetInterimExecutiveBoardCreated_DoesNotAffectOtherProperties()
+        {
+            // Arrange
+            var concern = new EngagementConcern(_testId, _supportProjectId, new EngagementConcernDetails());
+            concern.SetEngagementConcernDetails("Original details", "Summary", DateTime.Now.AddDays(-2));
+            concern.SetInformationPowersDetails(true, "Powers details", DateTime.Now.AddDays(-1));
+
+            var originalDetails = concern.EngagementConcernDetails;
+            var originalPowersInUse = concern.InformationPowersInUse;
+
+            // Act
+            concern.SetInterimExecutiveBoardCreated(true, "IEB details", DateTime.Now);
+
+            // Assert - Other properties remain unchanged
+            Assert.Equal(originalDetails, concern.EngagementConcernDetails);
+            Assert.Equal(originalPowersInUse, concern.InformationPowersInUse);
         }
     }
 }
