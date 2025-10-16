3a8eebf5886cbe2f6e3af32c122e5aba50ca5ce5
plockwood <paul.lockwood@nimbleapproach.com>
2025-10-16 12:45:10 +0100

    Remove HasAcknowledgedAndWillEngage from SupportProject
    - 🗑️ Removed the `HasAcknowledgedAndWillEngage` property from the `SupportProject` class.
    - 📉 Updated the model definition in `RegionalImprovementForStandardsAndExcellenceContextModelSnapshot.cs` to reflect the removal.
    - 🔄 Created migration `20251016113724_data-fix-dateConflictOfInterestDeclarationChecked.cs` to drop the column from the database and update related data.
    - 🛠️ Updated the designer file to include the new migration for schema consistency.

---
+++ src/Dfe.ManageSchoolImprovement.Infrastructure/Migrations/20251016113724_data-fix-dateConflictOfInterestDeclarationChecked.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.ManageSchoolImprovement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class datafixdateConflictOfInterestDeclarationChecked : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasAcknowledgedAndWillEngage",
+                schema: "RISE",
+                table: "SupportProject")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SupportProjectHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "RISE")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+
+            migrationBuilder.Sql(@"
+                UPDATE RISE.SupportProject
+                SET DateConflictOfInterestDeclarationChecked = ResponsibleBodyResponseToTheConflictOfInterestRequestReceivedDate");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasAcknowledgedAndWillEngage",
+                schema: "RISE",
+                table: "SupportProject",
+                type: "bit",
+                nullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "SupportProjectHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "RISE")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+    }
+}
