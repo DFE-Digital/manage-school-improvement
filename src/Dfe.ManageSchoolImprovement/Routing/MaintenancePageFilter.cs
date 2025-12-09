412d63e4eea26323a01d68eb1b84b868d9ad4082
plockwood <paul.lockwood@nimbleapproach.com>
2025-12-09 13:56:42 +0000

    Maintenance banner

---
+++ src/Dfe.ManageSchoolImprovement/Routing/MaintenancePageFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Dfe.ManageSchoolImprovement.Frontend.Routing
+{
+    public class MaintenancePageFilter : IAsyncPageFilter
+    {
+        private readonly IConfiguration _config;
+
+        public MaintenancePageFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context,
+                                                      PageHandlerExecutionDelegate next)
+        {
+            string? maintenanceModeValue = _config["MaintenanceMode"];
+
+            if (bool.TryParse(maintenanceModeValue, out bool maintenanceMode) && maintenanceMode &&
+                !string.IsNullOrEmpty(context.ActionDescriptor.DisplayName) &&
+                !context.ActionDescriptor.DisplayName.Contains("Maintenance"))
+            {
+                context.Result = new RedirectToPageResult("/public/maintenance");
+                return;
+            }
+
+            // Do post work.
+            await next.Invoke();
+        }
+    }
+}
