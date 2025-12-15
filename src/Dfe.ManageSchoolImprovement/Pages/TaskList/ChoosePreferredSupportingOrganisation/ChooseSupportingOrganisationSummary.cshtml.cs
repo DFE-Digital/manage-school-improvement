1b97fa1f4a4ebc6570f2af1bbe5837263a7b1c6f
plockwood <paul.lockwood@nimbleapproach.com>
2025-12-15 12:04:30 +0000

    - change to show date org choosen not date contact details confirmed

--- src/Dfe.ManageSchoolImprovement/Pages/TaskList/ChoosePreferredSupportingOrganisation/ChooseSupportingOrganisationSummary.cshtml.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/TaskList/ChoosePreferredSupportingOrganisation/ChooseSupportingOrganisationSummary.cshtml.cs
@@ -10,21 +10,21 @@ public class ChooseSupportOrganisationSummaryModel(
     : BaseSupportProjectPageModel(supportProjectQueryService, errorService), IDateValidationMessageProvider
 {
     public bool ShowError { get; set; }
-    
+
     public string? SupportOrganisationType { get; set; }
     public string? SupportOrganisationName { get; set; }
     public string? SupportOrganisationIdNumber { get; set; }
-    public DateTime? DateSupportingOrganisationContactDetailsAdded { get; set; }
+    public DateTime? DateSupportOrganisationChosen { get; set; }
 
     public async Task<IActionResult> OnGetAsync(int id, CancellationToken cancellationToken = default)
     {
         await base.GetSupportProject(id, cancellationToken);
-        
+
         SupportOrganisationType = SupportProject?.SupportOrganisationType;
         SupportOrganisationName = SupportProject?.SupportOrganisationName;
         SupportOrganisationIdNumber = SupportProject?.SupportOrganisationIdNumber;
-        DateSupportingOrganisationContactDetailsAdded = SupportProject?.DateSupportingOrganisationContactDetailsAdded;
-        
+        DateSupportOrganisationChosen = SupportProject?.DateSupportOrganisationChosen;
+
         return Page();
     }
 }
