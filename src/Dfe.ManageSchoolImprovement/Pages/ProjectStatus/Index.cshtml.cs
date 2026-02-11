6f367af4e9120dcf79c77f2c58b7281ff5d79287
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2026-02-11 13:55:46 +0000

    add calls to fetch project status audit data

--- src/Dfe.ManageSchoolImprovement/Pages/ProjectStatus/Index.cshtml.cs
+++ src/Dfe.ManageSchoolImprovement/Pages/ProjectStatus/Index.cshtml.cs
@@ -1,12 +1,16 @@
+using Dfe.Academisation.ExtensionMethods;
+using Dfe.ManageSchoolImprovement.Application.SupportProject.Models;
 using Dfe.ManageSchoolImprovement.Application.SupportProject.Queries;
 using Dfe.ManageSchoolImprovement.Domain.ValueObjects;
 using Dfe.ManageSchoolImprovement.Frontend.Models;
 using Dfe.ManageSchoolImprovement.Frontend.Services;
+using Dfe.ManageSchoolImprovement.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.ManageSchoolImprovement.Frontend.Pages.ProjectStatus;
 
 public class IndexModel(ISupportProjectQueryService supportProjectQueryService,
+    ISupportProjectAuditQueryService supportProjectAuditQueryService,
     IGetEstablishment getEstablishment,
     ErrorService errorService) : BaseSupportProjectEstablishmentPageModel(supportProjectQueryService, getEstablishment, errorService)
 {
@@ -15,6 +19,9 @@ public class IndexModel(ISupportProjectQueryService supportProjectQueryService,
     public ProjectStatusValue? SupportProjectStatus { get; set; }
     public DateTime? DateOfDecision { get; set; }
     public string? AdditionalDetails { get; set; }
+    
+    public IList<SupportProjectFieldAuditDto<string?>> ProjectStatusAuditTrail { get; set; } = [];
+
 
     public async Task<IActionResult> OnGetAsync(int id, CancellationToken cancellationToken)
     {
@@ -25,6 +32,56 @@ public class IndexModel(ISupportProjectQueryService supportProjectQueryService,
         SupportProjectStatus = SupportProject?.ProjectStatus;
         DateOfDecision = SupportProject?.ProjectStatusChangedDate;
         AdditionalDetails = SupportProject?.ProjectStatusChangedDetails;
+        
+        var auditTrail = new List<SupportProjectFieldAuditDto<string?>>();
+
+        // Get audit trail for project status
+        var projectStatusAuditResult = await supportProjectAuditQueryService.GetFieldAuditTrailAsync(
+            id, sp => sp.ProjectStatus.GetDisplayName(), cancellationToken);
+        
+        if (projectStatusAuditResult.IsSuccess)
+        {
+            var filteredProjectStatusAudits = projectStatusAuditResult.Value!
+                .Where(a => !string.IsNullOrEmpty(a.FieldValue))
+                .ToList();
+            auditTrail.AddRange(filteredProjectStatusAudits!);
+        }
+        
+        // Get audit trail for changed by
+        var changedByAuditResult = await supportProjectAuditQueryService.GetFieldAuditTrailAsync(
+            id, sp => sp.ProjectStatusChangedBy, cancellationToken);
+        
+        if (changedByAuditResult.IsSuccess)
+        {
+            var filteredChangedByAudits = changedByAuditResult.Value!
+                .Where(a => !string.IsNullOrEmpty(a.FieldValue))
+                .ToList();
+            auditTrail.AddRange(filteredChangedByAudits);
+        }
+        
+        // Get audit trail for date of decision
+        var dateOfDecisionAuditResult = await supportProjectAuditQueryService.GetFieldAuditTrailAsync(
+            id, sp => sp.ProjectStatusChangedDate.ToString(), cancellationToken);
+        
+        if (projectStatusAuditResult.IsSuccess)
+        {
+            var filteredDateOfDecisionAudits = dateOfDecisionAuditResult.Value!
+                .Where(a => !string.IsNullOrEmpty(a.FieldValue))
+                .ToList();
+            auditTrail.AddRange(filteredDateOfDecisionAudits);
+        }
+        
+        // Get audit trail for details
+        var detailsAuditResult = await supportProjectAuditQueryService.GetFieldAuditTrailAsync(
+            id, sp => sp.ProjectStatusChangedDetails, cancellationToken);
+        
+        if (detailsAuditResult.IsSuccess)
+        {
+            var filteredDetailsAudits = detailsAuditResult.Value!
+                .Where(a => !string.IsNullOrEmpty(a.FieldValue))
+                .ToList();
+            auditTrail.AddRange(filteredDetailsAudits);
+        }
 
         return Page();
     }
