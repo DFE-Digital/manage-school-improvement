a7c8f5222914ae8065cace74b577868f1ead901f
RichikaD <richika.test@gmail.com>
2026-03-16 13:23:15 +0000

    add tests for delete review

--- src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/delete-improvement-plan-objectives.cy.ts
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/delete-improvement-plan-objectives.cy.ts
@@ -33,13 +33,82 @@ describe('Delete improvement plan objective', () => {
         taskListActions.clickButton('save');
         taskList
             .navigateToTab('Improvement plan')
-        
+
         cy.executeAccessibilityTests();
-            
+
         improvementPlan
             .hasDeleteObjectiveLink()
             .deleteObjectiveSuccessfully();
 
         cy.executeAccessibilityTests();
+    });
+
+    it("should be able to delete progress reviews when no review has been recorded- Progress not recorded", () => {
+        taskList
+            .navigateToTab('Improvement plan')
+        improvementPlan
+            .ifObjectiveExist()
+
+        taskList
+            .navigateToTab('Task list')
+        taskList.selectTask("Enter improvement plan objectives");
+        taskListActions.addObjective();
+        taskListActions.selectButtonOrCheckbox("MarkAsComplete")
+        taskListActions.clickButton('save');
+
+        Logger.log("delete progress review");
+        taskList
+            .navigateToTab('Improvement plan')
+
+        improvementPlan
+            .clickRecordOrViewProgress()
+            .clickAddReview()
+            .fillReviewDetails()
+            .anotherReviewNeeded()
+            .hasStatusTag('Progress not recorded')
+            .hasEditOrDeleteReview()
+            .deleteReview()
+
+        cy.executeAccessibilityTests()
+    });
+
+    it.skip("should not be able to delete progress reviews when review has been recorded -Progress recorded", () => {
+        taskList
+            .navigateToTab('Improvement plan')
+        improvementPlan
+            .ifObjectiveExist()
+
+        taskList
+            .navigateToTab('Task list')
+        taskList.selectTask("Enter improvement plan objectives");
+        taskListActions.addObjective();
+        taskListActions.selectButtonOrCheckbox("MarkAsComplete")
+        taskListActions.clickButton('save');
+
+        Logger.log("delete progress review");
+        taskList
+            .navigateToTab('Improvement plan')
+
+        improvementPlan
+            .clickRecordOrViewProgress()
+            .clickAddReview()
+            .fillReviewDetails()
+            .anotherReviewNeeded()
+            .hasStatusTag('Progress partly recorded')
+            .hasEditReviewDetailsLink()
+            .clickRecordProgressLink()
+            .recordProgressForObjective()
+
+        cy.executeAccessibilityTests()
+
+        Logger.log("progress review cannot be deleted once recorded");
+        improvementPlan
+            .recordOverallProgress()
+            .returnToProgressReviews()
+            .hasEditReviewDetailsLink()
+            .clickEditReviewDetailsLink()
+            .noDeleteButtonExist()
+
+        cy.executeAccessibilityTests()
     });
 });
