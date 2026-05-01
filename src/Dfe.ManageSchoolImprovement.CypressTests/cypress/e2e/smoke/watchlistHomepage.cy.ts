89dfd1ad80c5d26de8381875ac4e1e64dc56bd94
RichikaD <richika.test@gmail.com>
2026-05-01 17:06:24 +0100

    watchlist test initial

--- src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/smoke/watchlistHomepage.cy.ts
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/e2e/smoke/watchlistHomepage.cy.ts
@@ -4,38 +4,37 @@ import { Logger } from "cypress/common/logger";
 describe("User lands in the watchlist homepage when loggedn in ", () => {
     beforeEach(() => {
         cy.login();
-         watchlistHomepage
-           .hasTitle('Your school watchlist')
-       
+        watchlistHomepage
+            .navigateToWatchlistHomepage()
+            .hasTitle('Your school watchlist')
+
         cy.executeAccessibilityTests()
+
+    });
+
+    it("should show a message when there are no schools assigned to the user", () => {
+        Logger.log("User friendly message show when there are no schools assigned to the user");
+        watchlistHomepage
+            .hasMessageWhenNoSchools('Your watchlist is currently empty.')
+
+        cy.executeAccessibilityTests()
+    });
+
+
+    it("should show only those schools which are assigned to the loggedin user", () => {
+        Logger.log("User friendly message show when there are no schools assigned to the user");
+        watchlistHomepage
+            .hasAssignedToValue()
 
     });
 
     it("should be able to find the schools list with details,assigned to the user", () => {
         Logger.log("User navigated to Task list");
         watchlistHomepage
-           .hasSchoolList()
-           .hasSchoolTableWithColumns(['School', 'Date added to MSI', 'Assigned to', 'Supporting organisation' , 'Milestone', 'Status']) 
-           .hasResultsWithDetails()
-           .hasHelpfulLinks()
-
-        cy.executeAccessibilityTests()
-    });
-
-    it("should show a message when there are no schools assigned to the user", () => {
-        Logger.log("User navigated to Task list");
-        watchlistHomepage
-           .hasSchoolList()
-           .hasMessageWhenNoSchools('Your watchlist is currently empty.')
-
-        cy.executeAccessibilityTests()
-    });
-   
-    it("should be able to navigate to the school when selected", () => {
-        Logger.log("User navigated to Task list");
-        watchlistHomepage
-           .hasSchoolList()
-           .clickFirstSchoolInList()
+            .hasSchoolList()
+            .hasSchoolTableWithColumns(['School', 'Date added to MSI', 'Assigned to', 'Supporting organisation', 'Milestone', 'Status'])
+            .hasResultsWithDetails()
+            .hasHelpfulLinks()
 
         cy.executeAccessibilityTests()
     });
@@ -44,20 +43,33 @@ describe("User lands in the watchlist homepage when loggedn in ", () => {
     it("should be able to navigate to the school when selected", () => {
         Logger.log("User navigated to Task list");
         watchlistHomepage
-           .hasSchoolList()
-           .removeFirstSchoolInList()
+            .hasSchoolList()
+            .clickFirstSchoolInList()
 
         cy.executeAccessibilityTests()
     });
 
-      it("should be able to sort the list, based on columns ", () => {
+
+    it("should be able to navigate to the school when selected", () => {
         Logger.log("User navigated to Task list");
         watchlistHomepage
-           .hasSchoolList()
-           .removeFirstSchoolInList()
+            .hasSchoolList()
+            .removeFirstSchoolInList()
 
         cy.executeAccessibilityTests()
-    });  
+    });
 
+    it("should be able to sort the list, based on columns ", () => {
+        Logger.log("User navigated to Task list");
+        watchlistHomepage
+            .hasSchoolList()
+            .sortByColumn('School')
+            .sortByColumn('Date added to MSI')
+            .sortByColumn('Assigned to')
+            .sortByColumn('Supporting organisation')
+            .sortByColumn('Milestone')
+            .sortByColumn('Status')
 
+        cy.executeAccessibilityTests()
+    });
 });    
--- src/Dfe.ManageSchoolImprovement.CypressTests/cypress/pages/watchlistHomepage.ts
+++ src/Dfe.ManageSchoolImprovement.CypressTests/cypress/pages/watchlistHomepage.ts
@@ -3,84 +3,99 @@
 class WatchlistHomepage {
 
 
-	public hasSchoolList() {
-		// Checks if the school list element exists on the page
-		cy.get('[data-testid="school-list"]').should('exist');
+    public navigateToWatchlistHomepage() {
+        cy.visit('/watchlist');
+        return this;
+    }
 
-        return this; // Allows for method chaining
-	}
+    public hasSchoolList() {
+        cy.get('[data-testid="school-list"]').should('exist');
 
-	hasSchoolTableWithColumns(columns: string[]) {
-		// Checks if the school table exists and has the specified columns
-		cy.get('[data-testid="school-table"]').should('exist');
-		columns.forEach(column => {
-			cy.get('[data-testid="school-table"] th').contains(column).should('exist');
-		});
+        return this;
+    }
 
-        return this; // Allows for method chaining
+    hasSchoolTableWithColumns(columns: string[]) {
+        // Checks if the school table exists and has the specified columns
+        cy.get('[data-testid="school-table"]').should('exist');
+        columns.forEach(column => {
+            cy.get('[data-testid="school-table"] th').contains(column).should('exist');
+        });
 
-	}
+        return this;
 
-       
-	public hasTitle(title: string) {
-		// Checks if the page contains the specified title
-		cy.get('h1').contains(title).should('exist');
-
-		return this;
-	}
-
-	public hasResultsWithDetails() {
-
-	  cy.get('[data-testid="school-table"] tbody tr').should('have.length.greaterThan', 0);
-
-	  cy.get('[data-testid="school-table"] tbody tr').each(($row) => {
-	  cy.wrap($row).find('td').each(($cell) => {
-	 cy.wrap($cell).invoke('text').should('not.be.empty');
-	 });
-    });
-		return this;
-	}
-
-	public hasHelpfulLinks() {
-		cy.get('[data-testid="helpful-links"]').should('exist').should('have.text', 'Helpful links (links open in a new tab)');
-
-		cy.get('[data-testid="helpful-links"]').parent().within(() => {
-			cy.contains('a', 'RISE delivery dashboard').should('exist');
-			cy.contains('a', 'RISE data tables').should('exist');
-			cy.contains('a', 'RISE monitoring report').should('exist');
-			cy.contains('a', 'See inspection reports at Ofsted').should('exist');
-			cy.contains('a', 'Get information about schools').should('exist');
-			cy.contains('a', 'Find information about schools and trusts').should('exist');
-            
-		});
+    }
 
 
-		return this;
-	}
+    public hasTitle(title: string) {
+        cy.get('h1').contains(title).should('exist');
+
+        return this;
+    }
+
+    public hasResultsWithDetails() {
+
+        cy.get('[data-testid="school-table"] tbody tr').should('have.length.greaterThan', 0);
+
+        cy.get('[data-testid="school-table"] tbody tr').each(($row) => {
+            cy.wrap($row).find('td').each(($cell) => {
+                cy.wrap($cell).invoke('text').should('not.be.empty');
+            });
+        });
+        return this;
+    }
+
+    public hasHelpfulLinks() {
+        cy.get('[data-testid="helpful-links"]').should('exist').should('have.text', 'Helpful links (links open in a new tab)');
+
+        cy.get('[data-testid="helpful-links"]').parent().within(() => {
+            cy.contains('a', 'RISE delivery dashboard').should('exist');
+            cy.contains('a', 'RISE data tables').should('exist');
+            cy.contains('a', 'RISE monitoring report').should('exist');
+            cy.contains('a', 'See inspection reports at Ofsted').should('exist');
+            cy.contains('a', 'Get information about schools').should('exist');
+            cy.contains('a', 'Find information about schools and trusts').should('exist');
+
+        });
+        return this;
+    }
 
 
     public hasMessageWhenNoSchools(message: string) {
-        cy.get('[data-testid="no-schools-message"]').should('exist').should('have.text', message);
+        cy.get('body').then($body => {
+            if ($body.find('table[data-testid="school-table"]').length > 0) {
+                cy.log('Schools are assigned to the user');
+            } else {
+                cy.get('p').should('exist').should('contain.text', message);
+            }
+        });
         return this;
     }
 
-    public clickFirstSchoolInList() {
-        cy.get('[data-testid="school-table"] tbody tr').first().click();
+    public hasAssignedToValue() {
+        cy.get('[data-testid="school-table"] tbody tr').each(($row) => {
+            cy.wrap($row).find('td').eq(2).invoke('text').then((text) => {
+                const trimmed = text.trim();
+                expect(trimmed).to.eq('TestFirstName TestLastName');
+            });
+        });
         return this;
-    }
 
-    public removeFirstSchoolInList() {
-        cy.get('[data-testid="school-table"] tbody tr').first().within(() => {
-            cy.get('[data-testid="remove-school-button"]').click();
-        }
-        );
-
-        return this;
     }
 
     public sortByColumn(columnName: string) {
         cy.get('[data-testid="school-table"] th').contains(columnName).click();
 
+        return this;
+    }
+
+
+    public clickFirstSchoolInList() {
+        cy.get('[data-testid="school-table"] tbody tr').first().find('td').first().find('a').click();
+        return this;
+    }
+
+    public removeFirstSchoolInList() {
+        cy.get('[data-testid="school-table"] tbody tr').first().find('td').last().find('button').click();
         return this;
     }
 
