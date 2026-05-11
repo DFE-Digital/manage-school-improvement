8643bf61a2f3cc9a4ec662c9b4e74d1e7f44400a
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2026-05-11 15:04:11 +0100

    add commands to remove all instances of school from watchlists when school is deleted

--- src/Dfe.ManageSchoolImprovement.Domain/Interfaces/Repositories/IWatchlistRepository.cs
+++ src/Dfe.ManageSchoolImprovement.Domain/Interfaces/Repositories/IWatchlistRepository.cs
@@ -1,10 +1,12 @@
 using Dfe.ManageSchoolImprovement.Domain.Entities.SupportProject;
+using Dfe.ManageSchoolImprovement.Domain.ValueObjects;
 
 namespace Dfe.ManageSchoolImprovement.Domain.Interfaces.Repositories
 {
     public interface IWatchlistRepository : IRepository<Watchlist>
     {
         Task<IEnumerable<Watchlist>> GetAllSchoolsForUser(string user, CancellationToken cancellationToken);
+        Task<IEnumerable<Watchlist>> GetAllWatchlistsForSchool(SupportProjectId supportProjectId, CancellationToken cancellationToken);
     }
 }
 
