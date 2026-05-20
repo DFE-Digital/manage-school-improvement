e06e458e410b1ab540dcc6c1e3b29de5007c423c
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2026-05-20 10:18:17 +0100

    fix failing build

--- src/Dfe.ManageSchoolImprovement.Domain/Interfaces/Repositories/IWatchlistRepository.cs
+++ src/Dfe.ManageSchoolImprovement.Domain/Interfaces/Repositories/IWatchlistRepository.cs
@@ -1,10 +1,14 @@
 using Dfe.ManageSchoolImprovement.Domain.Entities.SupportProject;
+using Dfe.ManageSchoolImprovement.Domain.ValueObjects;
 
 namespace Dfe.ManageSchoolImprovement.Domain.Interfaces.Repositories
 {
     public interface IWatchlistRepository : IRepository<Watchlist>
     {
         Task<IEnumerable<Watchlist>> GetAllSchoolsForUser(string user, CancellationToken cancellationToken);
+
+        Task<IEnumerable<Watchlist>> GetAllWatchlistsForSchool(SupportProjectId supportProjectId,
+            CancellationToken cancellationToken);
     }
 }
 
